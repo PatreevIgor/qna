4b92f8b7d5ebdbd6c817ea2285d7e831dce23690
PatreevIgor <igormanxxxx@yandex.ru>
2017-02-12 14:34:31 +0300

    Add test for action index Questions controller. Red status.

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+	describe 'GET #index' do
+	it 'populates an array of all questions' do
+		question1 = FactoryGirl.create(:question)
+		question2 = FactoryGirl.create(:question)
 
+		get :index
+		expect (assigns(:question)).to match_array([question1, question2]) # assigns(:question) return value @questions to index action QuestionController
+	end
+	it 'renders index view' do
+		get :index
+		expect(responce).to render_template :index
+	end
+	end
 end
