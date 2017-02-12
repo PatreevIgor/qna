2b798b4f22003bd5decf3eab2ce39cc75f338028
PatreevIgor <igormanxxxx@yandex.ru>
2017-02-12 15:29:15 +0300

    Refactoring. Add method let

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
 	describe 'GET #index' do
+		let(:questions) {create_list(:question, 2)}
+
 		before do
-			@questions = create_list(:question, 2)
 			get :index
 		end
+
 		it 'populates an array of all questions' do
-			expect(assigns(:questions)).to match_array(@questions) # assigns(:question) return value @questions to index action QuestionController
+			expect(assigns(:questions)).to match_array(questions) # assigns(:question) return value @questions to index action QuestionController
 		end
 
 		it 'renders index view' do
