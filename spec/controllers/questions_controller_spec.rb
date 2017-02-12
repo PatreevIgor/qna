7ea5075c87a2463d2185246a82047c613505eecc
PatreevIgor <igormanxxxx@yandex.ru>
2017-02-12 14:50:37 +0300

    Add variable to action index question controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
 	describe 'GET #index' do
-	it 'populates an array of all questions' do
-		question1 = FactoryGirl.create(:question)
-		question2 = FactoryGirl.create(:question)
+		it 'populates an array of all questions' do
+			question1 = FactoryGirl.create(:question)
+			question2 = FactoryGirl.create(:question)
 
-		get :index
-		expect (assigns(:question)).to match_array([question1, question2]) # assigns(:question) return value @questions to index action QuestionController
-	end
-	it 'renders index view' do
-		get :index
-		expect(response).to render_template :index
-	end
+			get :index
+			expect(assigns(:questions)).to match_array([question1, question2]) # assigns(:question) return value @questions to index action QuestionController
+		end
+		
+		it 'renders index view' do
+			get :index
+			expect(response).to render_template :index
+		end
 	end
 end
